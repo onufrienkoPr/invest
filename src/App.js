5801d4bf529a43640532edcbe0ca7ad5b45384c4
onufrienkoPr <artem_onufrienko@mail.ru>
2020-10-20 21:09:10 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="wrapper">
+                <div className="header">
+                    <div className="main-logo">
+                        Logo
+                    </div>
+                    <div className="header_main-menu">
+                        <ul className="main-menu">
+                            <li className="main-menu_item"><a>Инвестиционные идеи</a></li>
+                            <li className="main-menu_item"><a>Мой портфель</a></li>
+                        </ul>
+                    </div>
+                    <div className="header_user-menu">
+                        <div>
+                            <button>Вход</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
