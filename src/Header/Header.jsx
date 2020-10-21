76a8bfd962a605ecca11d7b632f1fbc5588a54ce
Artyom <44326834+onufrienkoPr@users.noreply.github.com>
2020-10-21 14:29:33 +0300

    Header Correction

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 import Login from "../Authentication/Login";
-import './Header.css'
+import logosite from "../assets/images/logo.png"
+import s from './Header.css'
 
 const Header = () => {
     return <div className="header">
         <div className="main-logo">
-            <div className="logo"><img src="https://illustrators.ru/uploads/attached_photo/image/8628/lord-king.png"/>
+            <div className="logo"><img src={logosite}/>
             </div>
-            <div><h1>Lord Invest</h1></div>
+            <div className="namesite"><p>RAZNESEM</p><p>STADIK</p></div>
         </div>
         <div className="header_main-menu">
             <ul className="main-menu">
+                <div className="link-hover-solid">
                 <li className="main-menu_item"><NavLink to={'/investments'}>Инвестиционные идеи</NavLink></li>
                 <li className="main-menu_item"><NavLink to={'/open-investments'}>Мой портфель</NavLink></li>
+                </div>
             </ul>
         </div>
         <Login/>
