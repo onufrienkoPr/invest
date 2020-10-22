ced63a98dcdf7882714cd4ef74cec8ae93f6f4bd
Artyom <44326834+onufrienkoPr@users.noreply.github.com>
2020-10-22 17:49:01 +0300

    idea card

--- src/components/Navbar_content/Investments/InvestmentsContainer.jsx
+++ src/components/Navbar_content/Investments/InvestmentsContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ContentContainer from "../../LinksMainInvests/LinksMainInvests";
-import './Investments.css'
+import s from './Investments.module.css'
+import spacex from "../../../assets/images/spacex.svg"
 
 const InvestmentsContainer = () => {
     let state = {
@@ -8,14 +9,21 @@ const InvestmentsContainer = () => {
     }
     return <>
         <ContentContainer state={state}/>
-        <div className="idea_list">
-            <div className="idea_list-cards">
-                <div className="idea_cards">1 контейнер</div>
-                <div className="idea_cards">2 контейнер</div>
-                <div className="idea_cards">3 контейнер</div>
-                <div className="idea_cards">4 контейнер</div>
-                <div className="idea_cards">5 контейнер</div>
-                <div className="idea_cards">6 контейнер</div>
+        <div className={s.idea_list}>
+            <div className={s.idea_list_cards}>
+                <div className={s.idea_cards}>
+                    <div className={s.irisk}></div>
+                    <div className={s.itype}>IPO</div>
+                    <div className={s.ilogo}><img src={spacex}/></div>
+                    <div className={s.iname}>SpaceX</div>
+                    <div className={s.idescription}>Полёты в космос и спутниковый интернет</div>
+                    <div className={s.iline}></div>
+                    <div className={s.ipricestart}>
+                        <div className={s.iprice}>Прогноз дохода<div className={s.iprices}>90 % годовых</div></div>
+                        <div className={s.istart}>Старт<div className={s.istarts}>21 окт 19:00</div></div>
+                    </div>
+                    <div className={s.ibutton}><button>Инвестировать от 50 $</button></div>
+                </div>
             </div>
         </div>
     </>
